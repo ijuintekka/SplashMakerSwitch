7232fdefc14ffbfdaec8b6df8a951b996de334dd
ijuintekka <ijuintekka@hotmail.com>
2018-07-26 11:27:33 +0100

    Initial commit

---
+++ SplashMakerSwitch/SplashMakerSwitch.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SplashMakerSwitch
+{
+    partial class SplashMakerSwitch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(149, 10);
+            this.Button1.Margin = new System.Windows.Forms.Padding(2);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(90, 28);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Save BIN";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBox1.Location = new System.Drawing.Point(9, 80);
+            this.PictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.PictureBox1.MinimumSize = new System.Drawing.Size(230, 249);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(230, 249);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox1.TabIndex = 1;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(9, 10);
+            this.Button2.Margin = new System.Windows.Forms.Padding(2);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(90, 28);
+            this.Button2.TabIndex = 2;
+            this.Button2.Text = "Import Image";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(11, 337);
+            this.Label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(95, 13);
+            this.Label1.TabIndex = 3;
+            this.Label1.Text = "Created by Tekka.";
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(9, 43);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(90, 28);
+            this.Button3.TabIndex = 4;
+            this.Button3.Text = "Import BIN";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(149, 43);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(90, 28);
+            this.Button4.TabIndex = 5;
+            this.Button4.Text = "Save PNG";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Label2
+            // 
+            this.Label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(114, 337);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(125, 13);
+            this.Label2.TabIndex = 6;
+            this.Label2.Text = "Canvas Size: 1280 x 720";
+            // 
+            // SplashMakerSwitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 364);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(266, 403);
+            this.Name = "SplashMakerSwitch";
+            this.Text = "SplashMaker";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Label Label2;
+    }
+}
+
---
+++ SplashMakerSwitch/SplashMakerSwitch.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SplashMakerSwitch
+{
+    public partial class SplashMakerSwitch : Form
+    {
+        Bitmap image = new Bitmap(1280, 720, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+        public SplashMakerSwitch()
+        {
+            InitializeComponent();
+            var graph = Graphics.FromImage(image);
+            graph.Clear(Color.Black);
+            PictureBox1.Image = image;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = "Saves As (*.bin)|*.bin";
+            if (svf.ShowDialog() == DialogResult.OK)
+            {
+                image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                List<byte> pixels = new List<byte>();
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        pixels.Add(pixel.R);
+                        pixels.Add(pixel.G);
+                        pixels.Add(pixel.B);
+                        pixels.Add(0);
+                    }
+                    for (int j = 0; j < 192; j++)
+                    {
+                        pixels.Add(0);
+                    }
+                }
+                File.WriteAllBytes(svf.FileName, pixels.ToArray());
+                image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose Image (*.bmp;*.gif;*.jpg;*.jpeg;*.png;*.tif;*.tiff)|*.bmp;*.gif;*.jpg;*.jpeg;*.png;*.tif;*.tiff";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                Image import = new Bitmap(opf.FileName);
+                var graph = Graphics.FromImage(image);
+                graph.Clear(Color.Black);
+                graph.DrawImage(import, (image.Width - import.Width) / 2, (image.Height - import.Height) / 2, import.Width, import.Height);
+                PictureBox1.Image = image;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose BIN (*.bin)|*.bin";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                List<byte> pixels = new List<byte>(File.ReadAllBytes(opf.FileName));
+                var graph = Graphics.FromImage(image);
+                graph.Clear(Color.Black);
+                int k = 0;
+                for (int i = 0; (i < image.Width) && (k < (pixels.Count)); i++)
+                {
+                    for (int j = 0; (j < image.Height) && (k < (pixels.Count)); j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        image.SetPixel(i, j, Color.FromArgb(255, pixels[k], pixels[k + 1], pixels[k + 2]));
+                        k = k + 4;
+                    }
+                    k = k + 192;
+                }
+                image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                PictureBox1.Image = image;
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = "Saves As (*.png)|*.png";
+            if (svf.ShowDialog() == DialogResult.OK)
+            {
+                image.Save(svf.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+    }
+}
